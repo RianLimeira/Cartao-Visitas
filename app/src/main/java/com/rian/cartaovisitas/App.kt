9e1581d0eda6bc33ffa5eee4e0fb80ec2e030586
RianLimeira <rianlimeira@gmail.com>
2022-08-08 14:38:36 -0300

    implementando binding

---
+++ app/src/main/java/com/rian/cartaovisitas/App.kt
@@ -0,0 +1,6 @@
+package com.rian.cartaovisitas
+
+import android.app.Application
+
+class App : Application() {
+}
---
+++ app/src/main/java/com/rian/cartaovisitas/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.rian.cartaovisitas.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.rian.cartaovisitas.R
+import com.rian.cartaovisitas.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
