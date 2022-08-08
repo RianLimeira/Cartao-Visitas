7b4628a4207e91452c0623992b5d83bfbbb23164
RianLimeira <rianlimeira@gmail.com>
2022-08-08 17:06:41 -0300

    getAllBusinessCard

--- app/src/main/java/com/rian/cartaovisitas/ui/MainActivity.kt
+++ app/src/main/java/com/rian/cartaovisitas/ui/MainActivity.kt
@@ -3,16 +3,28 @@ package com.rian.cartaovisitas.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import com.rian.cartaovisitas.R
+
+import androidx.activity.viewModels
+import com.rian.cartaovisitas.App
+
 import com.rian.cartaovisitas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
+    private val adapter by lazy { BusinessCardAdapter()}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+
+        getAllBusinessCard()
         insertListenner()
     }
 
@@ -22,4 +34,11 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this) { businessCard ->
+            adapter.submitList(businessCard)
+        }
+    }
+
 }
