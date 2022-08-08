3cc256817b5db506792d3f352f73866984a9529d
RianLimeira <rianlimeira@gmail.com>
2022-08-08 17:18:28 -0300

    Cards Inseridos

--- app/src/main/java/com/rian/cartaovisitas/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/com/rian/cartaovisitas/ui/AddBusinessCardActivity.kt
@@ -2,12 +2,22 @@ package com.rian.cartaovisitas.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.rian.cartaovisitas.App
+import com.rian.cartaovisitas.R
+import com.rian.cartaovisitas.data.BusinessCard
 import com.rian.cartaovisitas.databinding.ActivityAddBusinessCardBinding
 
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +30,16 @@ class AddBusinessCardActivity : AppCompatActivity() {
             finish()
         }
         binding.btnConfirmar.setOnClickListener {
-
+            val businessCard = BusinessCard(
+                nome = binding.tilNome.editText?.text.toString(),
+                empresa = binding.tilEmpresa.editText?.text.toString(),
+                telefone = binding.tilTelefone.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                fundoPesonalizado = binding.tilCor.editText?.text.toString()
+            )
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
